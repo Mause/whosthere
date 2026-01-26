503c511705f75b7a9d4aebf7926e86ce6cf32851
Elliana May <me@mause.me>
2026-01-26 16:37:45 +0700

    Update compat.go

--- cmd/compat.go
+++ cmd/compat.go
@@ -2,14 +2,22 @@
 
 package cmd
 
-/*
-#cgo LDFLAGS: -landroid
 // #include <android/api-level.h>
-// #include "cgo_helpers.h"
-*/
 import "C"
 
-func getApiLevel() int {
-	level := int(C.android_get_device_api_level())
-	return int(level)
+import "sync"
+
+var (
+	apiLevel int
+	once     sync.Once
+)
+
+// Returns the API level of the device we're actually running on, or -1 on failure.
+// The returned value is equivalent to the Java Build.VERSION.SDK_INT API.
+func androidDeviceApiLevel() int {
+	once.Do(func() {
+		apiLevel = int(C.android_get_device_api_level())
+	})
+
+	return apiLevel
 }
